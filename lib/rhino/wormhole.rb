478d81831ff6409ad0fa6edb4c17b2ca6909b4a3
Charles Lowell <cowboyd@thefrontside.net>
2011-10-05 16:49:51 -0500

    properly map Time objects.

--- lib/rhino/wormhole.rb
+++ lib/rhino/wormhole.rb
@@ -1,20 +1,21 @@
 
 module Rhino
-  module To        
+  module To
     JS_UNDEF = [J::Scriptable::NOT_FOUND, J::Undefined]
-    
+
     def ruby(object)
       case object
       when *JS_UNDEF                then nil
       when J::Wrapper               then object.unwrap
       when J::NativeArray           then array(object)
+      when J::NativeDate            then Time.at(object.getJSTimeValue() / 1000)
       when J::Regexp::NativeRegExp  then object
       when J::Function              then NativeFunction.new(object)
       when J::Scriptable            then NativeObject.new(object)
       else  object
-      end        
+      end
     end
-    
+
     def javascript(object)
       case object
       when String,Numeric       then object
@@ -22,12 +23,12 @@ module Rhino
       when Array                then J::NativeArray.new(object.to_java)
       when Hash                 then ruby_hash_to_native(object)
       when Proc,Method          then RubyFunction.new(object)
-      when NativeObject         then object.j      
+      when NativeObject         then object.j
       when J::Scriptable        then object
       else RubyObject.new(object)
       end
     end
-    
+
     def array(native)
       native.length.times.map {|i| ruby(native.get(i,native))}
     end
@@ -41,7 +42,7 @@ module Rhino
 
       native_object.j
 		end
-    
+
     module_function :ruby, :javascript, :array, :ruby_hash_to_native
   end
 end
