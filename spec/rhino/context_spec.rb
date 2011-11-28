436c7ad1293a7fd00560f4f906155a9ca68151f5
Charles Lowell <cowboyd@thefrontside.net>
2011-11-28 15:23:30 -0600

    fix error with poorly named constants

--- spec/rhino/context_spec.rb
+++ spec/rhino/context_spec.rb
@@ -1,9 +1,9 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 include Rhino
-  
-describe Rhino::Context do  
-  
+
+describe Rhino::Context do
+
   describe "Initalizing Standard Javascript Objects" do
     it "provides the standard objects without java integration by default" do
       Context.open do |cxt|
@@ -17,14 +17,14 @@ describe Rhino::Context do
         cxt["com"].should be_nil
       end
     end
-    
+
     it "provides unsealed standard object by default" do
       Context.open do |cxt|
         cxt.eval("Object.foop = 'blort'")
         cxt["Object"]['foop'].should == 'blort'
       end
     end
-    
+
     it "allows you to scope the context to an object" do
       class MyScope
         def foo; proc { 'bar' }; end
@@ -33,23 +33,23 @@ describe Rhino::Context do
         ctx.eval("foo()").should == 'bar'
       end
     end
-    
+
     it "allows you to seal the standard objects so that they cannot be modified" do
       Context.open(:sealed => true) do |cxt|
         lambda {
-          cxt.eval("Object.foop = 'blort'")            
-        }.should raise_error(Rhino::RhinoError)
-        
+          cxt.eval("Object.foop = 'blort'")
+        }.should raise_error(Rhino::JSError)
+
         lambda {
-          cxt.eval("Object.prototype.toString = function() {}")            
-        }.should raise_error(Rhino::RhinoError)          
+          cxt.eval("Object.prototype.toString = function() {}")
+        }.should raise_error(Rhino::JSError)
       end
     end
-    
+
     it "allows java integration to be turned on when initializing standard objects" do
       Context.open(:java => true) do |cxt|
           cxt["Packages"].should_not be_nil
       end
-    end    
+    end
   end
 end
