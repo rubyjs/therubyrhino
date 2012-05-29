rubyjs/therubyrhino

commit 6a0032e946a2c2a643ca4ae23c4c21e7c810bcc5

JSError updates - make sure cause always points to native rhino cause (closes #19) - correct javascript_backtrace to be an array (just like backtrace) - use a slicely customized stack trace rendering by default

lib/rhino/rhino_ext.rb
@@ -196,6 +196,21 @@ class Java::OrgMozillaJavascript::BaseFunction
   
 end
 
+class Java::OrgMozillaJavascript::ScriptStackElement
+  
+  def file_name; fileName; end # public final String fileName;
+  def function_name; functionName; end # public final String functionName;
+  def line_number; lineNumber; end # public final int lineNumber;
+  
+  def to_s
+    str = "at #{fileName}"
+    str << ':' << lineNumber.to_s if lineNumber > -1
+    str << " (#{funcionName})" if functionName
+    str
+  end
+  
+end
+
 class Java::OrgMozillaJavascript::Context
   
   CACHE = java.util.WeakHashMap.new

spec/rhino/error_spec.rb
@@ -59,5 +59,39 @@ describe Rhino::JSError do
       fail "expected to rescue"
     end
   end
+
+  it "contains the native error as the cause" do
+    begin
+      Rhino::Context.eval "throw 42"
+    rescue => e
+      e.cause.should_not be nil
+      e.cause.should be_a Java::OrgMozillaJavascript::JavaScriptException
+      e.cause.getValue.should == 42
+      e.cause.lineNumber.should == 1
+      e.cause.sourceName.should == '<eval>'
+    else
+      fail "expected to rescue"
+    end
+  end
+  
+  it "has a correct javascript backtrace" do
+    begin
+      Rhino::Context.eval "throw 42"
+    rescue => e
+      e.javascript_backtrace.should_not be nil
+      e.javascript_backtrace.should be_a Enumerable
+      e.javascript_backtrace.size.should == 1
+      e.javascript_backtrace[0].should == "at <eval>:1"
+      
+      e.javascript_backtrace(true).should be_a Enumerable
+      e.javascript_backtrace(true).size.should == 1
+      element = e.javascript_backtrace(true)[0]
+      element.file_name.should == '<eval>'
+      element.function_name.should be nil
+      element.line_number.should == 1
+    else
+      fail "expected to rescue"
+    end
+  end
   
 end

lib/rhino/error.rb
@@ -1,4 +1,3 @@
-
 module Rhino
 
   class JSError < StandardError
@@ -11,7 +10,14 @@ module Rhino
     
     # most likely a Rhino::JS::JavaScriptException
     def cause
-      @native.respond_to?(:cause) ? @native.cause : nil
+      return @cause if defined?(@cause)
+      @cause = begin
+        if @native.respond_to?(:cause) && @native.cause
+          @native.cause
+        else
+          @native.is_a?(JS::RhinoException) ? @native : nil
+        end
+      end
     end
 
     def value
@@ -40,9 +46,17 @@ module Rhino
       end
     end
     
-    def javascript_backtrace
-      cause.is_a?(JS::RhinoException) ? cause.getScriptStackTrace : nil
+    def javascript_backtrace(keep_elements = false)
+      if cause.is_a?(JS::RhinoException)
+        cause.getScriptStack.map do |element| # ScriptStackElement[]
+          keep_elements ? element : element.to_s
+        end
+      else
+        nil
+      end
     end
+    
+    Rhino::JS::RhinoException.useMozillaStackStyle(false)
     
   end
   

commit a38575a3d09fbde9b993942b68126939465ef51e

functionName typo introduced with 6a0032e946a2c2a643ca4ae23c4c21e7c810bcc5

lib/rhino/rhino_ext.rb
@@ -205,7 +205,7 @@ class Java::OrgMozillaJavascript::ScriptStackElement
   def to_s
     str = "at #{fileName}"
     str << ':' << lineNumber.to_s if lineNumber > -1
-    str << " (#{funcionName})" if functionName
+    str << " (#{functionName})" if functionName
     str
   end
   

spec/rhino/error_spec.rb
@@ -93,6 +93,18 @@ describe Rhino::JSError do
     end
   end
 
+  it "contains function name in javascript backtrace" do
+    begin
+      Rhino::Context.eval "function fortyTwo() { throw 42 }\n fortyTwo()"
+    rescue => e
+      e.javascript_backtrace.size.should == 2
+      e.javascript_backtrace[0].should == "at <eval>:1 (fortyTwo)"
+      e.javascript_backtrace[1].should == "at <eval>:2"
+    else
+      fail "expected to rescue"
+    end
+  end
+  
   it "backtrace starts with the javascript part" do
     begin
       Rhino::Context.eval "throw 42"