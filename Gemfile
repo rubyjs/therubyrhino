rubyjs/therubyrhino

commit db54adc223d8f5881fee81e533ae6ea419f27228

JSError improvement - make sure ruby error message is kept

lib/rhino/error.rb
@@ -4,8 +4,11 @@ module Rhino
     
     def initialize(native)
       @native = native # NativeException wrapping a Java Throwable
-      message = value ? value : ( cause ? cause.details : @native )
-      super(message)
+      if ( value = self.value(true) ) != nil
+        super value.is_a?(Exception) ? "#{value.class.name}: #{value.message}" : value
+      else
+        super cause ? cause.details : @native
+      end
     end
     
     def inspect
@@ -22,24 +25,10 @@ module Rhino
     # #Rhino::JS::JavaScriptException instance.
     def cause
       return @cause if defined?(@cause)
-      @cause = begin
-        if @native.respond_to?(:cause) && @native.cause
-          @native.cause
-        else
-          @native.is_a?(JS::RhinoException) ? @native : nil
-        end
-      end
-    end
-
-    # Return the thown (native) JavaScript value.
-    def value
-      return @value if defined?(@value)
-      if cause.respond_to?(:value) # e.g. JavaScriptException.getValue
-        @value = cause.value
-      elsif ( unwrap = self.unwrap ) && unwrap.respond_to?(:value)
-        @value = unwrap.value
+      if @native.respond_to?(:cause)
+        @native.cause
       else
-        @value = nil
+        @native.is_a?(JS::RhinoException) ? @native : nil
       end
     end
     
@@ -57,6 +46,14 @@ module Rhino
       else
         @unwrap = nil
       end
+    end
+    
+    # Return the thown (native) JavaScript value.
+    def value(unwrap = false)
+      return @value if defined?(@value) && ! unwrap
+      @value = get_value unless defined?(@value)
+      return @value.unwrap if unwrap && @value.respond_to?(:unwrap)
+      @value
     end
     
     # The backtrace is constructed using #javascript_backtrace + the Ruby part.
@@ -80,6 +77,18 @@ module Rhino
     end
     
     Rhino::JS::RhinoException.useMozillaStackStyle(false)
+    
+    private
+    
+    def get_value
+      if ( cause = self.cause ) && cause.respond_to?(:value)
+        cause.value  # e.g. JavaScriptException.getValue
+      elsif ( unwrap = self.unwrap ) && unwrap.respond_to?(:value)
+        unwrap.value
+      else
+        nil
+      end
+    end
     
   end
   

spec/rhino/error_spec.rb
@@ -128,6 +128,28 @@ describe Rhino::JSError do
     end
   end
 
+  it "wrapps false value correctly" do
+    begin
+      Rhino::Context.eval "throw false"
+    rescue => e
+      e.inspect.should == '#<Rhino::JSError: false>'
+      e.value.should be false
+    else
+      fail "expected to rescue"
+    end
+  end
+
+  it "wrapps null value correctly" do
+    begin
+      Rhino::Context.eval "throw null"
+    rescue => e
+      e.inspect.should == '#<Rhino::JSError: null>'
+      e.value.should be nil
+    else
+      fail "expected to rescue"
+    end
+  end
+  
   it "raises correct error from function#apply" do
     begin
       context = Rhino::Context.new
@@ -139,6 +161,33 @@ describe Rhino::JSError do
     else
       fail "expected to rescue"
     end
+  end
+
+  it "prints info about nested (ruby) error" do
+    context = Rhino::Context.new
+    klass = Class.new do
+      def hello(arg = 42) 
+        raise RuntimeError, 'hello' if arg != 42
+      end
+    end
+    context[:Hello] = klass.new
+    hi = context.eval "( function hi(arg) { Hello.hello(arg); } )"
+    begin
+      hi.call(24)
+    rescue => e
+      e.should be_a Rhino::JSError
+      e.value.should_not be nil
+      e.value.should be_a Rhino::Ruby::Object
+      e.value(true).should be_a RuntimeError # unwraps ruby object
+      # prints the original message (beyond [ruby RuntimeError]) :
+      e.message.should == "RuntimeError: hello"
+    else
+      fail "expected to rescue"
+    end
+    # V8::JSError: hello
+    #   from (irb):4:in `hello'
+    #   from at hi (<eval>:1:28)
+    #   from (irb):9
   end
   
 end

commit ce41ff620631811f1c47e24fd041bdccecfa132e

fix a regression introduced in db54adc223 + make sure bundler does not require 'less'

Gemfile
@@ -14,5 +14,5 @@ group :test do
   else
     gem 'therubyrhino_jar', :path => '.'
   end
-  gem 'less', '>= 2.2.1'
+  gem 'less', '>= 2.2.1', :require => nil
 end

lib/rhino/error.rb
@@ -25,7 +25,7 @@ module Rhino
     # #Rhino::JS::JavaScriptException instance.
     def cause
       return @cause if defined?(@cause)
-      if @native.respond_to?(:cause)
+      if @native.respond_to?(:cause) && @native.cause
         @native.cause
       else
         @native.is_a?(JS::RhinoException) ? @native : nil