8d49b6a37c5cf1a8d7f0ddbb7048412252f2e05d
Cameron Dutro <cdutro@twitter.com>
2013-06-14 11:01:40 -0700

    Avoiding reliance on Gem.loaded_specs

--- jar/rhino/jar_path.rb
+++ jar/rhino/jar_path.rb
@@ -1,7 +1,8 @@
 
 module Rhino
-  specs = Gem.loaded_specs["therubyrhino_jar"]
-  version = specs.version.to_s.split(".")
-  jar_file = "rhino-#{version[0]}.#{version[1]}R#{version[2]}.jar"
-  JAR_PATH = File.expand_path("../#{jar_file}", File.dirname(__FILE__))
+  JAR_VERSION = "1.7.4"
+  version_parts = JAR_VERSION.split(".")
+  current_dir = File.dirname(__FILE__)
+  jar_file = "rhino-#{version_parts[0]}.#{version_parts[1]}R#{version_parts[2]}.jar"
+  JAR_PATH = File.expand_path("../#{jar_file}", current_dir)
 end
